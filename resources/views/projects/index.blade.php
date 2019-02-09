f588010f25de740c0689989d012273b2ca7b5e09
vova <wolodymyr.ch@gmail.com>
2019-02-09 14:37:44 +0200

    episode #9

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,22 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="flex items-center">
-            <h1 class="mb-3 mr-auto">Projects</h1>
-            <a href="{{ route('projects.create') }}">Create a new project</a>
-        </div>
-        <ul>
-            @forelse($projects as $project)
-                <li>
-                    <a href="{{ $project->path() }}">
-                        {{ $project->title }}
-                    </a>
-                </li>
-            @empty
-                <p>No Projects yet.</p>
-            @endforelse
-        </ul>
+    <div class="flex items-center mb-3">
+        <h1 class="mb-3 mr-auto">Projects</h1>
+        <a href="{{ route('projects.create') }}">Create a new project</a>
+    </div>
+    <div class="flex">
+        @forelse($projects as $project)
+            <div class="bg-white mr-4 p-5 rounded shadow w-1/3" style="height: 200px;">
+                <h1 class="font-normal text-xl my-4">{{ $project->title }}</h1>
+                <div class="text-grey">{{ str_limit($project->description) }}</div>
+            </div>
+        @empty
+            <div>No Projects yet.</div>
+        @endforelse
     </div>
 @endsection
 
