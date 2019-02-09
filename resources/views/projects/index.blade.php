1278921189cb3b7fc12b9b12abb1de9aae0d4e90
vova <wolodymyr.ch@gmail.com>
2019-02-09 15:15:01 +0200

    episode #10

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,19 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex items-center mb-3">
-        <h1 class="mb-3 mr-auto">Projects</h1>
-        <a href="{{ route('projects.create') }}">Create a new project</a>
-    </div>
-    <div class="flex">
+    <header class="flex items-center mb-3 py-4">
+        <div class="flex justify-between items-center w-full">
+            <h2 class="mb-3 mr-auto text-nowrap text-grey text-sm">My Projects</h2>
+            <a href="{{ route('projects.create') }}" class="button">Create a new project</a>
+        </div>
+    </header>
+    <main class="lg:flex lg:flex-wrap -mx-3">
         @forelse($projects as $project)
-            <div class="bg-white mr-4 p-5 rounded shadow w-1/3" style="height: 200px;">
-                <h1 class="font-normal text-xl my-4">{{ $project->title }}</h1>
-                <div class="text-grey">{{ str_limit($project->description) }}</div>
+            <div class="lg:w-1/3 px-3 pb-6">
+                <div class="bg-white p-5 rounded shadow" style="height: 200px;">
+                    <h1 class="font-normal text-xl py-4 pl-4 -ml-5 mb-3 border-blue-light border-l-4">
+                        <a href="{{ $project->path() }}" class="text-black no-underline">{{ $project->title }}</a>
+                    </h1>
+                    <div class="text-grey">{{ str_limit($project->description) }}</div>
+                </div>
             </div>
         @empty
             <div>No Projects yet.</div>
         @endforelse
-    </div>
+    </main>
 @endsection
 
