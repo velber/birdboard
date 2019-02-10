ded675f81f5b9b398af186c0e70840749c3e86fa
vova <wolodymyr.ch@gmail.com>
2019-02-10 16:23:14 +0200

    episode #15

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,6 +8,8 @@ class Task extends Model
 {
     protected $guarded = ['id'];
 
+    protected $touches = ['project'];
+
     public function project()
     {
         return $this->belongsTo(Project::class);
