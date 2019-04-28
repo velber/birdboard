e3401d40cbe3dc3f37c3f62068f7d5698115788e
vova <wolodymyr.ch@gmail.com>
2019-04-28 14:24:26 +0300

    episode #39, add vue-js modal component

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -16,5 +16,34 @@
             <div>No Projects yet.</div>
         @endforelse
     </main>
+    <modal name="hello-world" classes="p-10 bg-card rounded-lg" height="auto">
+        <h1 class="font-normal mb-16 text-center text-2xl">Create project</h1>
+        <div class="flex">
+            <div class="flex-1 mr-4">
+                <div class="mb-4">
+                    <label for="title" class="text-sm block mb-2">Title</label>
+                    <input type="text" id="title" class="border border-muted-light p-2 text-xs block w-full rounded">
+                </div>
+                <div class="mb-4">
+                    <label for="description" class="text-sm block mb-2">Description</label>
+                    <textarea type="description" id="title" class="border border-muted-light p-2 text-xs block w-full rounded" rows="7"></textarea>
+                </div>
+            </div>
+            <div class="flex-1 ml-4">
+                <div class="mb-4">
+                    <label for="title" class="text-sm block mb-2">Need new?</label>
+                    <input type="text" id="title" class="border border-muted-light p-2 text-xs block w-full rounded" placeholder="Task 1">
+                </div>
+                <button class="inline-flex items-center text-xs">
+                    <span>Add New Task Field</span>
+                </button>
+            </div>
+        </div>
+        <footer class="flex justify-end">
+            <button class="button mr-3 is-outlined">Cancel</button>
+            <button class="button">Create Project</button>
+        </footer>
+    </modal>
+    <a href="" @click.prevent="$modal.show('hello-world')">Show Modal</a>
 @endsection
 
